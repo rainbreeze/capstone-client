4535cef6d6ef5150b604c0fc9f3db6cc8f8915b7
hyun <hyunxx.seo@gmail.com>
2025-04-06 22:00:51 +0900

    game container 준비

---
+++ src/components/gamepage/GameScene.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Phaser from "phaser"
+
+export default class GameScene extends Phaser.Scene {
+
+    constructor() {
+        super("GameScene")
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+}
---
+++ src/components/homepage/GameContainer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function GameContainer() {
+
+}
+export default GameContainer;
