afae92bbef1c1677d10ae824cf6e85284aa4a9b6
joji <rainbreeze2@naver.com>
2025-04-07 16:48:24 +0900

    Eslint 문제 해결 3

--- src/components/gamepage/GameScene.js
+++ src/components/gamepage/GameScene.js
@@ -7,7 +7,6 @@ export default class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        // 타일맵과 타일셋 로딩
         this.load.tilemapTiledJSON('map', 'assets/tilemaps/map_test.json');
         this.load.image('forestTown', 'assets/tilesets/tiles_packed.png');
         this.load.image('snowExpansion', 'assets/tilesets/snow-expansion.png');
@@ -15,16 +14,16 @@ export default class GameScene extends Phaser.Scene {
 
     create() {
         console.log('create start');
-        
-        // 타일맵 생성
+
         const map = this.make.tilemap({ key: "map" });
 
         // 타일셋을 사용하여 맵의 레이어 생성
         const tileset = map.addTilesetImage('forestTown');
-        // layer는 나중에 사용 시 다시 추가
+        //  const bridge = map.addTilesetImage('snowExpansion');
+        // 더 이상 layer를 사용하지 않음
+        map.createLayer('Tile Layer 1', tileset, 0, 0);
     }
 
     update() {
-        // 게임 씬에서 매 프레임마다 업데이트할 내용이 없다면 빈 함수로 둡니다.
     }
 }
